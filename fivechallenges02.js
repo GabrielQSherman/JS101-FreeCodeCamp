f2d9b5de12884bc41093df65e715a50c62848119
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-11 16:03:05 -0400

    Add files via upload
    Learning about increments and decrements as well as decimals in JavaScript

---
+++ fivechallenges02.js
@@ -0,0 +1,24 @@
+//Incrementing and decrementing
+
+var myVar0 = 87;
+// This is going to add 1 to the value of myVar one time, 
+//this is useful for loops and counting
+myVar0++; // or ++myVar
+
+var myVar1 = 11;
+// value will go down by one, expected value is 10
+myVar1--; //or --myVar
+
+//Decimals
+var ourDecimal = 5.7;
+// challenge is to create your own decimal
+var myDecimal = 3.1415926535;
+
+//FCC wants you to make decimal product that equals 5
+var product = 2.0 * 0.0;
+var myProduct = 4.0 * 1.25;
+
+//FCC wants you to make decimal product that equals 2.2
+var quotient = 0.0 / 2.0;
+var myQuotient = 4.4 / 2.0;
+
