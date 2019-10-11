c61bf07a070e5e66005efc38c3697dd89f785947
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-11 15:36:52 -0400

    Add files via upload
    5 (actually 6) FCC challenges that explore initializing variables, camel case and case sensitivity with variable declarations, and the basic operators of numbers

---
+++ fivechallenges01.js
@@ -0,0 +1,50 @@
+
+//First challenge is to initialize the variables 
+//so the correct output is returned
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Understand that variables are strickly case sensitive 
+// it is important to camel case variable names to keep code clean and eaisly readable
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// one of the data types frquently used in JavaScript is the 'Number' data type 
+// this challange has us modifiying the initialization of the var 'sum'
+//the '+' is used for additon
+var sum = 10 + 10;
+//expected output is 20
+
+//another operator is '-' or subtraction
+var diffrence = 45 - 33;
+//expected output is 12
+
+//another operator is '*' or multipication
+var product = 8 * 10;
+//expected output is 80
+
+//another operator is '/' or division
+var quotient = 66 / 33;
+//expected output is 2
+
+//output in terminal for what I learned
+//console.log(sum,diffrence, product, quotient, a, b, c);
+
+
+
