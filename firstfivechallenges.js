a2460d89a7931da84bd01d287167fcb7c155678e
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-11 13:26:47 -0400

    Add files via upload
    The first 5 challenges from FreeCodeCamp.org

---
+++ firstfivechallenges.js
@@ -0,0 +1,35 @@
+// This is called a single line comment
+
+/*
+
+this is a multi-line comment
+ha
+ha
+ha
+
+*/
+
+// this is how one declares a variable pre ES6
+
+var myName; // = "gabe"
+
+// this is how variable assignment can be used very simply
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
+
+//this is how one can initialize a variable while declaring it
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+var a = 9;
+
+
