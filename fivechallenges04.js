4b678c7cb01f63709ec619920bc3a72993b91c71
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-12 12:14:49 -0400

    Add files via upload
    This set deals with escape characters and different ways strings can be assigned and declared in JS

---
+++ fivechallenges04.js
@@ -0,0 +1,24 @@
+//the first challenge is to delare a new String variable
+
+var myFirstName = "gabriel";
+var myMiddleName = "quinn";
+
+//next is to use escape character for the first time
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//next is to make a link work without using the escape character '\'
+//this shows that escaping '"' (double quotes) wasnt neccecary because the string was contained within a single quote pair
+var myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//next challenge is to create their desired display of  
+/*
+FirstLine
+    \SecondLine
+ThirdLine
+*/
+var myParagraph = "FirstLine\n\t\\Secondline\nThirdLine";
+
+//next challenge is to concatinate strings
+
+var myConcatStr = "This is the start. " + "This is the end.";
