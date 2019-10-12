5ee623cf293cb8c607e241aeee26ff2bc1be984e
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-12 11:03:21 -0400

    Add files via upload
    learning to use remainders and learning to shorten math operators code with the 'operator=' method of addition, subtraction, multiplication, and division.

---
+++ fivechallenges03.js
@@ -0,0 +1,45 @@
+// in JavaScript we can find the remainder of a quotient by using the % operator
+/* Tip;
+17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is Even)
+*/
+
+var remainder = 11 % 3;
+
+// a very useful way to shorten code is to use the '"operator"=' to make code like a = a + 2 to look like a+=2. they both do the same thing
+
+var a = 3;
+var b = 17;
+var c = 12;
+// FCC wants to modify the code so it uses '+='
+a += 12;
+b += 9;
+c += 7;
+
+//this logic can be aplied to any mathimatical operator in JS
+var a1 = 11;
+var b1 = 9;
+var c1 = 3;
+//now with the subraction operator
+a1 -= 6;
+b1 -= 15;
+c1 -= 1;
+
+//Now with the multiplication operator
+var a2 = 5;
+var b2 = 12;
+var c2 = 4.6;
+a2 *= 5;
+b2 *= 3;
+c2 *= 10;
+
+//Now with the division operator
+var a = 48;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+
