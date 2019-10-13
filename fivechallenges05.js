716bffa1004fed78340cbb09ec24016cf0036e82
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-13 10:24:42 -0400

    Add files via upload
    5 challenges that explores the use and syntax of Strings in JS.

---
+++ fivechallenges05.js
@@ -0,0 +1,12 @@
+//This challenge teaches that you can concatenate strings with the same 'plus equals' that we previously used on number equations
+ var myStr = "This is the first sentence. ";
+ myStr += "This is the second sentence.";
+
+ //There is also another way to concat and apend strings, as well as variables of any kind with ' "string" + variableName '
+ var myName = "gabe";
+ var myNameStr =  "My name is " + myName + " and I am well!"
+ 
+ //This challenge combines the last two!
+ var someAdjective = "fantastic";
+ var myStr = "Learning to code is ";
+ myStr += someAdjective;
