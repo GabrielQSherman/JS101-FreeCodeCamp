e4eab44dcb0be9fe17c90042d6819fa06677d1ef
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-16 11:41:55 -0400

    Add files via upload

---
+++ fivechallenges08.js
@@ -0,0 +1,6 @@
+//the .push() method is very useful for minipulating arrays
+//here they would like me to add a [dog, 3] subarray to this two dimensional array
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+
+//the .pop() method allows you to remove an item at the end of an array or subarray
